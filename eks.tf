5d2988fab1cc694d170ab81a11ae8268ca785b6c
Ed Wantuil <ed@wantuil.com>
2024-03-16 15:47:14 -0300

    Remove Aurora master credentials from push.yml and add eks.tf
    This commit removes the use of Aurora master username and password in the Terraform setup steps of the Github Actions workflow, push.yml. Additionally, it adds a new Terraform configuration file, eks.tf, for managing EKS (Elastic Kubernetes Service) resources on AWS.

---
+++ eks.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-east-2"
+}
+#
+#data "aws_ami" "eks_worker" {
+#  filter {
+#    name   = "eks-techchallenge"
+#    values = ["amazon-eks-node-v1.18.8-20200610"]
+#  }
+#
+#  most_recent = true
+#  owners      = ["602401143452"] # Amazon
+#}
+#
+#module "eks" {
+#  source          = "terraform-aws-modules/eks/aws"
+#  cluster_version = "1.18"
+#  subnets         = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+#  vpc_id          = "vpc-abcde012"
+#
+#  node_groups = {
+#    eks_nodes = {
+#      desired_capacity = 2
+#      max_capacity     = 2
+#      min_capacity     = 1
+#
+#      instance_type = "m4.large"
+#      key_name      = "eks"
+#
+#      root_block_device_size = "50"
+#
+#      source_security_group_ids = ["sg-0123456abcde7890a"]
+#
+#      additional_tags = {
+#        Environment = "production"
+#        Name        = "eks-worker-node"
+#      }
+#    }
+#  }
+#}
