06edd5c99c1c9c0595c8136ab3b88f09cdab02a4
Ed Wantuil <ed@wantuil.com>
2024-03-16 15:52:19 -0300

    Removed image scanning configuration in ECR repository
    This commit removes the 'image_scanning_configuration' block from the 'aws_ecr_repository' resource in the ecr.tf Terraform file. The 'scan_on_push' attribute was turned on previously, and has now been removed.

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,4 @@
 resource "aws_ecr_repository" "techchallenge_repo" {
   name                 = "techchallenge"
   image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 }
