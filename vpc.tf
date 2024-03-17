4302999898fba58b39a24c1d9c1cb087b9c5b2e1
Ed Wantuil <ed@wantuil.com>
2024-03-16 22:33:26 -0300

    Update Terraform files for EKS and Secrets Manager
    Moved from EKS AMI and module to EKS security group and cluster creation in `eks.tf`. Also, uncommented the Secrets Manager resources and added JSON configuration in `secrets.tf`. Made a small update in `.github/workflows/push.yml`.

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,53 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "eks-vpc"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+  }
+}
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnets)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.private_subnets[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = false
 
   tags = {
+    Name = "private-${var.azs[count.index]}"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-${var.azs[count.index]}"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  count         = var.enable_nat_gateway ? length(var.public_subnets) : 0
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  allocation_id = aws_eip.nat[count.index].id
+  depends_on    = [aws_internet_gateway.igw]
+}
+
+resource "aws_eip" "nat" {
+  count = var.enable_nat_gateway ? length(var.public_subnets) : 0
+  vpc   = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "eks-igw"
     "kubernetes.io/cluster/eks-cluster" = "shared"
   }
 }
