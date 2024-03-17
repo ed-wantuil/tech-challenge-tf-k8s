2791218f2634f444744e848df8e2f676fc8e454f
Ed Wantuil <ed@wantuil.com>
2024-03-16 22:56:44 -0300

    Comment out 'aws_secretsmanager_secret_version' in secrets.tf
    The 'aws_secretsmanager_secret_version' used for the techchallenge has been commented out in the 'secrets.tf' file. This will prevent its usage temporarily, possibly due to security considerations or testing.

--- secrets.tf
+++ secrets.tf
@@ -2,10 +2,11 @@ resource "aws_secretsmanager_secret" "techchallenge" {
   name        = "techchallenge"
   description = "Vault do techchallenge"
 }
-resource "aws_secretsmanager_secret_version" "techchallenge" {
-  secret_id     = aws_secretsmanager_secret.techchallenge.id
-  secret_string = jsonencode({
-    username = var.database_username
-    password = var.database_password
-  })
-}
+
+#resource "aws_secretsmanager_secret_version" "techchallenge" {
+#  secret_id     = aws_secretsmanager_secret.techchallenge.id
+#  secret_string = jsonencode({
+#    username = var.database_username
+#    password = var.database_password
+#  })
+#}
