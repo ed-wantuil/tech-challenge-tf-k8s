958d4493f31061492c226131be955e470d914996
Ed Wantuil <ed@wantuil.com>
2024-03-16 23:13:52 -0300

    Update subnet IDs in 'eks.tf'
    The subnet IDs within the 'vpc_config' of 'eks.tf' file have been updated. The previous subnet IDs were replaced by new ones as part of the ongoing infrastructure modification.

--- eks.tf
+++ eks.tf
@@ -27,7 +27,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = "arn:aws:iam::058264138215:role/LabRole"
 
   vpc_config {
-    subnet_ids         = ["subnet-12345", "subnet-67890"]
+    subnet_ids         = ["	subnet-056963b6d47b4b04a", "subnet-084f14e43b8753e6c"]
     security_group_ids = [aws_security_group.eks_sg.id]
   }
 }
