cb8287c3d9a30d0b75b20997cd04affa35350daf
Ed Wantuil <ed@wantuil.com>
2024-03-16 22:59:01 -0300

    Update: Commented out 'aws_secretsmanager_secret_version' in secrets.tf
    The 'aws_secretsmanager_secret_version' resource for the tech challenge is now temporarily disabled in the 'secrets.tf' file. This is likely for security reasons or for testing purposes, as this change effectively halts its use.

--- secrets.tf
+++ secrets.tf
@@ -2,7 +2,7 @@ resource "aws_secretsmanager_secret" "techchallenge" {
   name        = "techchallenge"
   description = "Vault do techchallenge"
 }
-
+#
 #resource "aws_secretsmanager_secret_version" "techchallenge" {
 #  secret_id     = aws_secretsmanager_secret.techchallenge.id
 #  secret_string = jsonencode({
