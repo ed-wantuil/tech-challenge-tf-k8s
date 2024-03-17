82e120ec2cc0dc8b35193f2b28071e99ab80f728
Ed Wantuil <ed@wantuil.com>
2024-03-16 22:38:05 -0300

    Add variables file for Terraform configuration
    This commit introduces a new file, `variables.tf`, which holds the configurable variables for Terraform. Variables such as cluster name, EKS version, database username, master password, private and public subnets, availability zones and NAT gateway enabling option are included. These changes help to make the Terraform configurations more flexible and scalable.

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "cluster_name" {
+  default = "eks_cluster"
+}
+
+variable "eks_version" {
+  default = "1.21"
+}
+
+variable "database_username" {}
+
+variable "master_password" {}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT gateway"
+  type        = bool
+  default     = true
+}
