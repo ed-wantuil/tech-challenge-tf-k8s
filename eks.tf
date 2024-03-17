9a4027a3e3083f6a409bb44e0626701fdf97503e
Ed Wantuil <ed@wantuil.com>
2024-03-17 15:21:59 -0300

    Add EKS node group to eks.tf
    Added a new AWS EKS node group resource to eks.tf, specifying node group name, node role ARN, subnet IDs and instance types, along with scaling configurations. This allows for more granular and effective control over the AWS EKS cluster's compute resources.

--- eks.tf
+++ eks.tf
@@ -8,3 +8,20 @@ resource "aws_eks_cluster" "eks_cluster" {
     security_group_ids = ["sg-0afbb6ab8e72f0925"]
   }
 }
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "node_eks"
+  node_role_arn   = "arn:aws:iam::058264138215:role/LabRole"
+  subnet_ids      = ["subnet-056963b6d47b4b04a", "subnet-084f14e43b8753e6c"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [aws_eks_cluster.eks_cluster]
+}
