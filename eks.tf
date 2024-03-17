5474e16adc67f801a7801f96d8ae96603c127677
Ed Wantuil <ed@wantuil.com>
2024-03-16 23:20:05 -0300

    Update subnet and security group in 'eks.tf'
    The VPC configuration in 'eks.tf' has been updated with specific subnet IDs and a direct reference to the security group ID. Rather than pulling these values from the existing data structure, they are now explicitly defined within the 'vpc_config' block which simplifies the file and reduces unnecessary dependencies.

--- eks.tf
+++ eks.tf
@@ -4,7 +4,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = "arn:aws:iam::058264138215:role/LabRole"
 
   vpc_config {
-    subnet_ids         = ["	subnet-056963b6d47b4b04a", "subnet-084f14e43b8753e6c"]
-    security_group_ids = [aws_security_group.eks_sg.id]
+    subnet_ids         = ["subnet-056963b6d47b4b04a", "subnet-084f14e43b8753e6c"]
+    security_group_ids = ["sg-0afbb6ab8e72f0925"]
   }
 }
