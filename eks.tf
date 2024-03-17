4428a07105b6deea62d438a0d865485b61557863
Ed Wantuil <ed@wantuil.com>
2024-03-17 14:01:28 -0300

    Update EKS Fargate subnet IDs and remove secret creation
    The EKS Fargate subnet IDs in the eks.tf file have been explicitly defined rather than dynamically retrieved. The resources for AWS Secrets Manager secret creation and versioning have been commented out from the secrets.tf file. This change reduces the risk of incorrect dynamic subnet ID retrieval and pauses the creation of new secrets until further notice.

--- eks.tf
+++ eks.tf
@@ -15,7 +15,7 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
   fargate_profile_name   = "fargate"
   pod_execution_role_arn = "arn:aws:iam::058264138215:role/LabRole"
 
-  subnet_ids = ["subnet-056963b6d47b4b04a", "subnet-084f14e43b8753e6c"]
+  subnet_ids = ["subnet-09a3317c6c79b710e"]
 
   selector {
     namespace = "kube-system"
